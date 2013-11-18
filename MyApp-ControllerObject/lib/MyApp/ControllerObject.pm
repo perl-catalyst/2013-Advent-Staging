d8afde78492ef0a39adb0915aca26db837aa2da9
John Napiorkowski <jjnapiork@cpan.org>
2013-11-18 16:01:35 -0600

    finished up the first draft

--- MyApp-ControllerObject/lib/MyApp/ControllerObject.pm
+++ MyApp-ControllerObject/lib/MyApp/ControllerObject.pm
@@ -5,14 +5,23 @@ use warnings;
 
 use Moose::Exporter;
 use Import::Into;
-use Module::Runtime qw(use_module);
+use Module::Runtime;
 
 my ($import, $unimport, $init_meta) =
   Moose::Exporter->build_import_methods( also => ['Moose'] );
 
+sub importables {
+  'Function::Parameters',
+  'CatalystX::Syntax::Action',
+  'namespace::autoclean',
+}
+
+sub base_class { 'Catalyst::Controller' }
+
 sub init_meta {
   my ($class, @args) = @_;
-  Moose->init_meta( @args, base_class => 'Catalyst::Controller' );
+  Moose->init_meta( @args,
+    base_class => $class->base_class );
   goto $init_meta if $init_meta;
 }
 
@@ -20,12 +29,6 @@ sub import {
   use_module($_)->import::into(scalar caller)
     for shift->importables;
   goto $import;
-}
-
-sub importables {
-  'Function::Parameters',
-  'CatalystX::Syntax::Action',
-  'namespace::autoclean',
 }
 
 1;
